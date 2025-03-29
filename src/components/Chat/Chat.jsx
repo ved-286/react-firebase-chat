a36b1be9a9ca9fe48ac071e5424b6b995d53da79
unknown <patelved1921@gmail.com>
2025-03-29 05:16:26 -0700

    designed the Chat section and handled the text input

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,9 +1,24 @@
 import './chat.css'
 import React from 'react'
 import EmojiPicker from 'emoji-picker-react'
+import { BsSend } from "react-icons/bs";
 
 const Chat = () => {
-  const [open,setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false)
+  const [text , setText] = React.useState('')
+
+
+  const handleEmojiClick = (emojiData) => {
+    setText(prev => prev + emojiData.emoji)
+    setOpen(false)
+  }
+
+  const HandleSend = () => {
+    if(text.length > 0) {
+      console.log(text)
+      setText('')
+    }
+  }
   return (
     <div className='chat'>
       <div className="top">
@@ -20,19 +35,60 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+            <span>Just Now</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+            <span>Just Now</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+            <span>Just Now</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+            <span>Just Now</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+            <span>Just Now</span>
+          </div>
+        </div>
+      </div>
       <div className="bottom">
+        <div className="emoji">
+          <img src="./emoji.png" alt="" onClick={() => setOpen(prev => !prev)} />
+          {open && (
+            <div className="pickerContainer">
+              <EmojiPicker open={open} onEmojiClick={handleEmojiClick} />
+            </div>
+          )}
+        </div>
+        <input type="text"
+        onChange={e => setText(e.target.value)}
+        value={text} 
+        placeholder='Type a Message....' />
         <div className="icons">
           <img src="./img.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
-        <input type="text" placeholder='Type a Message....' />
-        <div className="emoji">
-          <img src="./emoji.png" alt="" onClick={()=>{setOpen(prev=>!prev)}} />
-          <EmojiPicker open={open}/>
-          <div className="sendButton">
-            <button>send</button>
-          </div>
+        <div className="sendButton">
+        <BsSend Fill onClick={HandleSend} />
         </div>
       </div>
     </div>
